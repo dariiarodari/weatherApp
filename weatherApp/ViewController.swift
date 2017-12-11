97c2d12526492bf583f76fd12038259cff7a6f6d
Dariia Rodari <dariiarodari@gmail.com>
2017-12-11 21:48:15 +0200

    Initial Commit

---
+++ weatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Dariia Rodari on 12/11/17.
+//  Copyright © 2017 rodariapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
